0af64fe5c2c2e87714c50dda47c81c79686310e3
jonascr86 <jonascr86@gamil.com>
2022-04-12 21:59:51 -0300

    Configurações iniciais da aplicação, desenvolvimento do cabeçalho.

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts, 
+  Archivo_400Regular,
+  Archivo_500Medium
+} from '@expo-google-fonts/archivo'
+import { 
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+} from '@expo-google-fonts/inter'
+
+import { Home } from './src/sreens/Home';
+import theme from './src/styles/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home/>
+    </ThemeProvider>
+  )
+}
