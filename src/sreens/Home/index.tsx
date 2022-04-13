4bf07fc9b88cfb4647b34c3bcbaecdf97f77092d
jonascr86 <jonascr86@gamil.com>
2022-04-12 23:01:15 -0300

    Componente de cartão do Carro

--- src/sreens/Home/index.tsx
+++ src/sreens/Home/index.tsx
@@ -9,8 +9,27 @@ import {
 
 import Logo from '../../assets/logo.svg'
 import { RFValue } from 'react-native-responsive-fontsize'
+import { Car } from '../../components/Car'
 
 export function Home(){
+    const CarDataOne = {
+        brand: 'Audi',
+        name: 'RS 5 Coupé',
+        rent:{
+            period: 'ao dia',
+            price: 120,
+        },
+        thumbnail: 'https://w7.pngwing.com/pngs/853/38/png-transparent-2017-audi-r8-car-audi-rs5-audi-r8-lms-2016-audi-sedan-car-performance-car.png',
+    }
+    const CarDataTwo = {
+        brand: 'porche',
+        name: 'Panamera',
+        rent:{
+            period: 'ao dia',
+            price: 350,
+        },
+        thumbnail: 'https://img1.gratispng.com/20180704/yoz/kisspng-2014-bmw-3-series-bmw-328-bmw-7-series-car-porsche-panamera-5b3d5a4fa36463.0701901515307474716693.jpg',
+    }
     return(
         <Container>
             <StatusBar
@@ -29,6 +48,12 @@ export function Home(){
                     </TotalCars>
                 </HeaderContent>
             </Header>
+            <Car 
+               data={CarDataOne}
+            />
+            <Car 
+               data={CarDataTwo}
+            />
         </Container>
     )
 }
