4259dc9f07274557381202b1d6ba8aa52ce02ea3
jonascr86 <jonascr86@gamil.com>
2022-05-09 21:19:50 -0300

    Adissionado uma SplashScreen

--- src/sreens/Splash/index.tsx
+++ src/sreens/Splash/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { Dimensions, StatusBar, StyleSheet, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'  
 
 import {
     Container,
 } from './styles'
+const WIDTH = Dimensions.get('window').width;
 
 export function Spalsh(){
-    
+    const animated = useSharedValue(0)
+    const animatedStyle = useAnimatedStyle(() => {
+        return{
+            transform: [
+                {
+                    translateX: withTiming(animated.value, {
+                        duration: 500,
+                        easing: Easing.bezier
+                    }),
+                }
+            ]
+        }
+    })
+    function handleAnimation(){
+        animated.value = Math.random() * (WIDTH - 100);
+    }
     return(
         <Container>
             <StatusBar 
@@ -14,6 +32,35 @@ export function Spalsh(){
                 backgroundColor="transparent"
                 translucent
             />
+
+            <Animated.View style={[styles.box, animatedStyle]} >
+
+            </Animated.View>
+            <RectButton
+                style={styles.button}
+                onPress={handleAnimation}
+            >
+                <Text style={styles.textButton} >Press</Text>
+            </RectButton>
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    box:{
+        backgroundColor: '#FFF',
+        width: 100,
+        height: 100,
+    },
+    button:{
+        backgroundColor: '#322665',
+        width: 300,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton:{
+        fontSize: 30,
+        color: '#FFF',
+    }
+})
