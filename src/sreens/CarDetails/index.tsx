e8ba7af9bdda5ed67b3030063c8887418bf55133
jonascr86 <jonascr86@gamil.com>
2022-05-09 22:26:51 -0300

    Iniciando animação do cabeçalho do cardetail

--- src/sreens/CarDetails/index.tsx
+++ src/sreens/CarDetails/index.tsx
@@ -25,6 +25,8 @@ import { Button } from "../../components/Button";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { CarDTO } from "../../dtos/CarDTO";
 import { getAccessoryIcon } from "../../utils/getAccessoryIcon";
+import Animated, { Extrapolate, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 interface Props{
     car: CarDTO
@@ -34,6 +36,19 @@ export function CarDetails(){
     const navigation = useNavigation();
     const route = useRoute();
     const { car } = route.params as Props;
+    const scrollY = useSharedValue(0);
+    const scrollHandle = useAnimatedScrollHandler(event => {
+        scrollY.value = event.contentOffset.y;
+        console.log(event.contentOffset.y)
+    })
+
+    const headerStyleAnimation = useAnimatedStyle(() => {
+        return{
+            height: interpolate(
+                scrollY.value,[0, 200], [200, 70], Extrapolate.CLAMP
+            )
+        }
+    })
     
     function handlerShowScheduling(){
         navigation.navigate({name: 'Scheduling', params: {car}});
@@ -45,19 +60,32 @@ export function CarDetails(){
     return(
         <Container>
             <StatusBar
-                barStyle='light-content'
+                barStyle='dark-content'
+                translucent
+                backgroundColor="transparent"
             />
-            <Header>
-                <BackButton 
-                    onPress={handlerGoBack}
-                />
-            </Header>
-            <CarImages>
-                <ImageSlider 
-                    imageUrl={car.photos}
-                />
-            </CarImages>
-            <Content>
+            <Animated.View
+                style={[headerStyleAnimation]}
+            >
+                <Header>
+                    <BackButton 
+                        onPress={handlerGoBack}
+                    />
+                </Header>
+                <CarImages>
+                    <ImageSlider 
+                        imageUrl={car.photos}
+                    />
+                </CarImages>
+            </Animated.View>
+            <Animated.ScrollView
+                contentContainerStyle={{
+                    paddingHorizontal: 24,
+                    paddingTop: getStatusBarHeight(),
+                }}
+                showsVerticalScrollIndicator={false}
+                onScroll={scrollHandle}  
+            >
                 <Details>
                     <Description>
                         <Brand>{ car.brand }</Brand>
@@ -80,8 +108,19 @@ export function CarDetails(){
                         ))
                     }
                 </Accessories>
-                <About>{car.about}</About>
-            </Content>
+                <About>
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                    {car.about}
+                </About>
+            </Animated.ScrollView>
             <Footer>
                 <Button title={"Escolher período do aluguel"} onPress={handlerShowScheduling} />
             </Footer>
